b18bf4be1c029669ec4d884a8fa4de22523c782e
Victoria <vickimarji@icloud.com>
2022-07-19 16:56:21 -0400

    easy

---
+++ app.js
@@ -0,0 +1,16 @@
+function bouncer(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) newArray.push(arr[i]);
+    }
+    return newArray;
+  }
+
+
+  function chunkArrayInGroups(arr, size) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+      newArr.push(arr.slice(i, i + size));
+    }
+    return newArr;
+  }
